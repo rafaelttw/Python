0c161cf06f1b1288f4b26b1c72b49bebae695c5c
rafaelttw <rafaelttw@gmail.com>
2018-02-14 13:58:56 -0200

    Add files via upload

---
+++ enderecoip.py
@@ -0,0 +1,12 @@
+ip = raw_input('Digite um endereco IP: ')
+print 'O endereco ' + ip + ' e um enderco valido!'
+print 'O endereco %s e um enderco valido!'% ip
+print 'O endereco %s tem o tamanho de %d caracteres' %(ip,len(ip))
+print 'O endereco %s tem o tamanho de %s caracteres' %(ip,str(len(ip)))
+print 'Numero quebrado %f' % 1.123214214
+print 'Numero quebrado %.2f' % 1.123214214
+#resumo ate aqui
+#para declarar numeros interios usamos %d.
+#para declarar strings usamos %s
+#para declarar numeros quebrados usamos %f
+#para restringir o numero de casas usamos %.2f -> por exemplo duas casas
